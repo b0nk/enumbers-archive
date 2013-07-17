6a142ba3b715acae995bdb4c0815d748ec1256e3
Oleksiy Voronin <ovoronin@gmail.com>
2013-07-16 21:28:06 -0400

    Styling rework
    Set target SDK to 17.
    Added ActionBarSherlock
    Reworked search and list handling
    Removed support for global search
    Change main icon

--- src/org/uaraven/e/ECodeAdapter.java
+++ src/org/uaraven/e/ECodeAdapter.java
@@ -7,149 +7,103 @@
 
 package org.uaraven.e;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
-import android.database.DataSetObserver;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
+import net.ninjacat.android.list.HolderCreator;
+import net.ninjacat.android.list.ListViewUtils;
+import net.ninjacat.android.list.UiHolder;
 
-public class ECodeAdapter implements ListAdapter, ECodeListObserver {
-	
-	private ECodeList list;
-	private List<DataSetObserver> observers;
-	private LayoutInflater inflater;
-	private Context context;
+public class ECodeAdapter extends BaseAdapter implements HolderCreator<ECode> {
 
-	public ECodeAdapter(Context context, ECodeList list) {
-		this.list = list;
-		this.context = context;
-		this.observers = new ArrayList<DataSetObserver>();
-		inflater = LayoutInflater.from(context);
-		
-		this.list.setObserver(this);
-	}
+    private ECodeList list;
+    private Context context;
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View v;
-		if (convertView == null)
-			v = inflater.inflate(R.layout.ecode, null);
-		else
-			v = convertView;
-		
-		ECode code = list.get(position);		
-		
-		TextView tvcode = (TextView) v.findViewById(R.id.ecode);
-		TextView tvname = (TextView) v.findViewById(R.id.name);
-		TextView tvpurpose = (TextView) v.findViewById(R.id.purpose);
-		TextView tvstatus = (TextView) v.findViewById(R.id.status);
-		ImageView ivegan = (ImageView) v.findViewById(R.id.veganIcon);
-		ImageView ichild = (ImageView) v.findViewById(R.id.childIcon);
-		ImageView iallerg = (ImageView) v.findViewById(R.id.allergyIcon);
-		ImageView iextra = (ImageView) v.findViewById(R.id.iIcon);
-		
-		
-		tvcode.setBackgroundColor(code.getColor());
-		tvcode.setText("E" + code.eCode);
-		tvname.setText(code.name);
-		tvpurpose.setText(code.purpose);
-		tvstatus.setText(getCaption(code));
+    public ECodeAdapter(Context context, ECodeList list) {
+        this.list = list;
+        this.context = context;
+    }
 
-		ivegan.setImageResource(code.vegan == 0 ? R.drawable.v : 
-			(code.vegan == 2 ? R.drawable.v_y : R.drawable.v_r));
-		ichild.setImageResource(code.children == 0 ? R.drawable.ch : R.drawable.ch_r);
-		iallerg.setImageResource(code.allergic ? R.drawable.al_r : R.drawable.al);
-		
-		/*ivegan.setVisibility(code.safeForVegans() ? View.VISIBLE: View.GONE);
-		ichild.setVisibility(code.safeForChildren() ? View.VISIBLE: View.GONE);
-		iallerg.setVisibility(code.safeForAllergic() ? View.VISIBLE: View.GONE);*/
-		iextra.setVisibility(code.hasExtra() ? View.VISIBLE: View.GONE);		
-		
-		return v;
-	}
-	
-	public String getCaption(ECode code) {
-		switch (code.getDanger()) {
-		case ECode.PERMITTED:
-			return context.getString(R.string.cap_permitted);
-		case ECode.UNPERMITTED:
-			return context.getString(R.string.cap_unpermitted);
-		case ECode.MOSTLY_SAFE:
-			return context.getString(R.string.cap_forbidden);
-		case ECode.DANGEROUS:
-			return context.getString(R.string.cap_dangerous);
-		}
-		return context.getString(R.string.cap_unknown);
-	}	
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-	@Override
-	public boolean areAllItemsEnabled() {
-		return true;
-	}
+        UiHolder<ECode> holder = ListViewUtils.createHolder(context, this, R.layout.ecode, convertView);
+        ECode code = list.get(position);
 
-	@Override
-	public boolean isEnabled(int position) {
-		return true;
-	}
+        return holder.fillView(code);
+    }
 
-	@Override
-	public int getCount() {
-		return list.size();
-	}
+    @Override
+    public int getCount() {
+        return list.size();
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return list.get(position);
-	}
+    @Override
+    public ECode getItem(int position) {
+        return list.get(position);
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-	@Override
-	public int getItemViewType(int position) {
-		return 0;
-	}
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 
-	@Override
-	public int getViewTypeCount() {
-		return 1;
-	}
+    @Override
+    public UiHolder<ECode> createHolder(View view) {
+        return new CodeDataHolder(view);
+    }
 
-	@Override
-	public boolean hasStableIds() {
-		return true;
-	}
+    private static final class CodeDataHolder extends UiHolder<ECode> {
 
-	@Override
-	public boolean isEmpty() {
-		return list.size() == 0;
-	}
+        private final View band;
+        private final TextView codeId;
+        private final TextView codeName;
+        private final TextView codePurpose;
+        private final ImageView imageVegan;
+        private final ImageView imageChild;
+        private final ImageView imageAllergy;
 
-	@Override
-	public void registerDataSetObserver(DataSetObserver observer) {
-		observers.add(observer);
-	}
+        private CodeDataHolder(View view) {
+            super(view);
 
-	@Override
-	public void unregisterDataSetObserver(DataSetObserver observer) {
-		int idx = observers.indexOf(observer);
-		if (idx >=0)
-			observers.remove(idx);
-	}
+            band = view.findViewById(R.id.color_band);
+            codeId = (TextView) view.findViewById(R.id.ecode);
+            codeName = (TextView) view.findViewById(R.id.name);
+            codePurpose = (TextView) view.findViewById(R.id.purpose);
+            imageVegan = (ImageView) view.findViewById(R.id.vegan);
+            imageChild = (ImageView) view.findViewById(R.id.child);
+            imageAllergy = (ImageView) view.findViewById(R.id.allergy);
+        }
 
-	@Override
-	public void dataChanged() {
-		for (DataSetObserver observer: observers)
-			observer.onChanged();
-	}
-	
-	
+        @Override
+        public View fillView(ECode eCode) {
+            band.setBackgroundColor(eCode.getColor());
+            codeId.setText("E" + eCode.eCode);
+            codeName.setText(eCode.name);
+            codePurpose.setText(eCode.purpose);
+
+            if (eCode.hasExtra()) {
+                codeId.setBackgroundColor(0xFFF0F0FF);
+            } else {
+                codeId.setBackgroundColor(android.R.color.background_light);
+            }
+
+            imageVegan.setImageResource(eCode.vegan == 0 ? R.drawable.v : (eCode.vegan == 2 ? R.drawable.v_y : R.drawable.v_r));
+            imageChild.setImageResource(eCode.children == 0 ? R.drawable.ch : R.drawable.ch_r);
+            imageAllergy.setImageResource(eCode.allergic ? R.drawable.al_r : R.drawable.al);
+
+            return view;
+        }
+
+
+    }
+
 }
