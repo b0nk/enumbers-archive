0337825bcad8d7810c0d860eaf284af5592bd51f
Oleksiy Voronin <ovoronin@gmail.com>
2013-07-17 20:21:53 -0400

    Slight layout changes, especially for SDK levels 8-10

--- src/org/uaraven/e/ECodeViewActivity.java
+++ src/org/uaraven/e/ECodeViewActivity.java
@@ -80,20 +80,6 @@ public class ECodeViewActivity extends Activity {
                 finish();
             }
         });
-
     }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-////                Intent intent = new Intent(this, EMainActivity.class);
-////                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-////                startActivity(intent);
-//                finish();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
+
 }
